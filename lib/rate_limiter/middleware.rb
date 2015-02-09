c869ee110ea85c3058300a18509e1db2519bfbaf
Dawid Warpas <dawid.warpas@gmail.com>
2015-02-09 14:37:50 +0100

    [#2] add limit option and test for it

--- lib/rate_limiter/middleware.rb
+++ lib/rate_limiter/middleware.rb
@@ -1,12 +1,13 @@
 module RateLimiter
   class Middleware
     def initialize(app, options = {})
-      @app = app
+      @app   = app
+      @limit = options[:limit] || 60
     end
 
     def call(env)
       status, headers, response = @app.call(env)
-      headers["X-RateLimit-Limit"] = "60"
+      headers["X-RateLimit-Limit"] = @limit
       [status, headers, response]
     end
   end
--- spec/rate_limiter_spec.rb
+++ spec/rate_limiter_spec.rb
@@ -15,4 +15,8 @@ describe RateLimiter do
   it 'should have the right header' do
     expect(last_response.header).to include("X-RateLimit-Limit")
   end
+
+  it 'should accept the passed limit' do
+    expect(last_response.header).to include("X-RateLimit-Limit" => 100)
+  end
 end
