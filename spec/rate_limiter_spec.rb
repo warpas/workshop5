0496d9424de41652f61985ef9df51b08e5faa6b8
Dawid Warpas <dawid.warpas@gmail.com>
2015-02-17 15:19:44 +0100

    [#7] add red test for distinguishing clients by api_token, ditch Rack::Builder for now

--- spec/rate_limiter_spec.rb
+++ spec/rate_limiter_spec.rb
@@ -3,11 +3,8 @@ require 'timecop'
 
 describe RateLimiter do
   let(:app) do
-    Rack::Builder.app do
-      use Rack::Lint
-      use RateLimiter::Middleware, limit: 100, reset_in: 7200
-      run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']] }
-    end
+    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']] }
+    Rack::Lint.new(RateLimiter::Middleware.new(app, { limit: 100, reset_in: 7200 }) { |env| Rack::Request.new(env).params["api_token"] })
   end
 
   before { get '/' }
@@ -48,6 +45,15 @@ describe RateLimiter do
       expect(last_response.header).to include("X-RateLimit-Remaining" => "97")
       4.times { get '/', {}, "REMOTE_ADDR" => "10.0.0.1" }
       expect(last_response.header).to include("X-RateLimit-Remaining" => "92")
+    end
+
+    it 'should have seperate values for differently distinguished clients' do
+      2.times { get '/', { 'api_token' => '47FUfXweoM9MlRev3LHTahi6' } }
+      expect(last_response.header).to include("X-RateLimit-Remaining" => "98")
+      5.times { get '/', { 'api_token' => 'bo2qc2tHNikfPmehfxTz2wBt' } }
+      expect(last_response.header).to include("X-RateLimit-Remaining" => "95")
+      4.times { get '/', {}, "REMOTE_ADDR" => "10.0.0.1" }
+      expect(last_response.header).to include("X-RateLimit-Remaining" => "96")
     end
 
     describe 'Reset' do
